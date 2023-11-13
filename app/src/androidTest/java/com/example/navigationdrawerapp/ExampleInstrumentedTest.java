97ab96e9716742a86ca51d4c1a643aec1cf3c0ff
Benedict Contawe <benedictcontawe@gmail.com>
2023-11-13 20:19:06 +0800

    Major update for Java Right Drawer Branch

--- app/src/androidTest/java/com/example/navigationdrawerapp/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/navigationdrawerapp/ExampleInstrumentedTest.java
@@ -1,9 +1,9 @@
 package com.example.navigationdrawerapp;
 
 import android.content.Context;
+
 import androidx.test.platform.app.InstrumentationRegistry;
-//import androidx.test.ext.junit.runners.AndroidJUnit4;
-import androidx.test.runner.AndroidJUnit4;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -20,8 +20,7 @@ public class ExampleInstrumentedTest {
     @Test
     public void useAppContext() {
         // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
-
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
         assertEquals("com.example.navigationdrawerapp", appContext.getPackageName());
     }
 }
--- app/src/main/java/com/example/navigationdrawerapp/DashboardActivity.java
+++ app/src/main/java/com/example/navigationdrawerapp/DashboardActivity.java
@@ -1,23 +1,25 @@
 package com.example.navigationdrawerapp;
 
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
 import android.app.Activity;
 import android.os.Bundle;
-import com.google.android.material.navigation.NavigationView;
-
-import androidx.drawerlayout.widget.DrawerLayout;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.material.navigation.NavigationView;
+
 public class DashboardActivity extends Activity implements View.OnClickListener {
 
-    DrawerLayout drawerLayout;
-    ImageView navigationBar,iv_logout;
-    LinearLayout ll_First,ll_Second,ll_Third,ll_Fourth,ll_Fifth,ll_Sixth,ll_Seventh;
-    NavigationView navigationView;
-    TextView tv_logout;
+    private DrawerLayout drawerLayout;
+    private ImageView navigationBar,iv_logout;
+    private LinearLayout ll_First,ll_Second,ll_Third,ll_Fourth,ll_Fifth,ll_Sixth,ll_Seventh;
+    private NavigationView navigationView;
+    private TextView tv_logout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +32,8 @@ public class DashboardActivity extends Activity implements View.OnClickListener 
     private void onSetNavigationDrawerEvents() {
         drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
         navigationView = (NavigationView) findViewById(R.id.navigationView);
+
+        drawerLayout.openDrawer(GravityCompat.END);
 
         navigationBar = (ImageView) findViewById(R.id.navigationBar);
         ll_First = (LinearLayout) findViewById(R.id.ll_First);
@@ -56,52 +60,65 @@ public class DashboardActivity extends Activity implements View.OnClickListener 
     }
 
     @Override
-    public void onClick(View v) {
-        switch (v.getId()){
+    public void onClick(View view) {
+        /*
+        switch (view.getId()) {
             case R.id.navigationBar:
-                drawerLayout.openDrawer(navigationView, true);
                 break;
             case R.id.ll_First:
-                showToast("ll_First");
-                drawerLayout.closeDrawer(navigationView, true);
                 break;
             case R.id.ll_Second:
-                showToast("ll_Second");
-                drawerLayout.closeDrawer(navigationView, true);
                 break;
             case R.id.ll_Third:
-                showToast("ll_Third");
-                drawerLayout.closeDrawer(navigationView, true);
                 break;
             case R.id.ll_Fourth:
-                showToast("ll_Fourth");
-                drawerLayout.closeDrawer(navigationView, true);
                 break;
             case R.id.ll_Fifth:
-                showToast("ll_Fifth");
-                drawerLayout.closeDrawer(navigationView, true);
                 break;
             case R.id.ll_Sixth:
-                showToast("ll_Sixth");
-                drawerLayout.closeDrawer(navigationView, true);
                 break;
             case R.id.ll_Seventh:
-                showToast("ll_Seventh");
-                drawerLayout.closeDrawer(navigationView, true);
                 break;
             case R.id.iv_logout:
-                showToast("iv_logout");
-                drawerLayout.closeDrawer(navigationView, true);
                 break;
             case R.id.tv_logout:
-                showToast("tv_logout");
-                drawerLayout.closeDrawer(navigationView, true);
                 break;
             default:
-                showToast("Default");
-                drawerLayout.closeDrawer(navigationView, true);
                 break;
-
+        }
+        */
+        if (view.getId() == R.id.navigationBar) {
+            drawerLayout.openDrawer(navigationView, true);
+        } else if (view.getId() == R.id.ll_First) {
+            showToast("ll_First");
+            drawerLayout.closeDrawer(navigationView, true);
+        } else if (view.getId() == R.id.ll_Second) {
+            showToast("ll_Second");
+            drawerLayout.closeDrawer(navigationView, true);
+        } else if (view.getId() == R.id.ll_Third) {
+            showToast("ll_Third");
+            drawerLayout.closeDrawer(navigationView, true);
+        } else if (view.getId() == R.id.ll_Fourth) {
+            showToast("ll_Fourth");
+            drawerLayout.closeDrawer(navigationView, true);
+        } else if (view.getId() == R.id.ll_Fifth) {
+            showToast("ll_Fifth");
+            drawerLayout.closeDrawer(navigationView, true);
+        } else if (view.getId() == R.id.ll_Sixth) {
+            showToast("ll_Sixth");
+            drawerLayout.closeDrawer(navigationView, true);
+        } else if (view.getId() == R.id.ll_Seventh) {
+            showToast("ll_Seventh");
+            drawerLayout.closeDrawer(navigationView, true);
+        } else if (view.getId() == R.id.iv_logout) {
+            showToast("iv_logout");
+            drawerLayout.closeDrawer(navigationView, true);
+        } else if (view.getId() == R.id.tv_logout) {
+            showToast("tv_logout");
+            drawerLayout.closeDrawer(navigationView, true);
+        } else {
+            showToast("Default");
+            drawerLayout.closeDrawer(navigationView, true);
         }
     }
 
